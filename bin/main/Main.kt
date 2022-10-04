3104adab4b6028caae55095ad718dc9aa1ecec72
diego <diegobadillo@live.com>
2022-10-03 19:59:19 -0500

    Retomando curso de Kotlin

--- bin/main/Main.kt
+++ bin/main/Main.kt
@@ -1,3 +1,22 @@
+const val PI = 3.14159
 fun main(args: Array<String>) {
     println("Hello World!")
+    var dinero =  10
+    println(dinero)
+    dinero = 5
+    println(dinero)
+    val nombre = "Maria<3"
+    println(nombre)
+    println(PI)
+
+
+    val boolean = true
+    val numeroLargo = 3L
+    val double : Double = 2.7185
+    val float : Float = 1.1f
+
+    val primervalor =  20
+    val segundoValor = 10
+    val tercerValor = primervalor - segundoValor
+    println(tercerValor)
 }
