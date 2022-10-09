a29c89dc29a3287d0b01b98d9fc44bab23232afa
diego <diegobadillo@live.com>
2022-10-09 15:24:16 -0500

    Elvis Operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,5 @@
 fun main(args: Array<String>) {
     var nombre : String? = null
-    try {
-        throw NullPointerException("Referencia nula")
-    } catch (excepcion : NullPointerException){
-        println("Ha ocurrido un error")
-    } finally {
-        println("Finalmente ah ocurrido un error, cerrando aplicacion")
-    }
-
-    val primerValor = 10
-    val segundoValor = 0
-    val resultado : Int = try { primerValor/segundoValor } catch (exception : Exception) { 0 }
-    println(resultado)
+    val caracteresDeNombre : Int = nombre?.length ?: 0
+    println(caracteresDeNombre)
 }
