908a48ae8f9c1e2903d67066f39323d4cab64652
diego <diegobadillo@live.com>
2022-10-09 15:38:08 -0500

    Listas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,34 @@
 fun main(args: Array<String>) {
-    var nombre : String? = null
-    val caracteresDeNombre : Int = nombre?.length ?: 0
-    println(caracteresDeNombre)
+    //Lista inmutable, no se pueden cambiar los valores
+    val listasDeNombres = listOf("Juan", "Enrique", "Camila")
+    println(listasDeNombres)
+
+    //Lista mutable, si se pueden cambiar los valores
+    val listaVacia = mutableListOf<String>()
+    println(listaVacia)
+
+    listaVacia.add("Juan")
+    println(listaVacia)
+
+    val valorUsandoGet = listaVacia.get(0)
+    println(valorUsandoGet)
+
+    val valorUsandoOPerador = listaVacia[0]
+    println(valorUsandoOPerador)
+
+    val primerValor: String? = listasDeNombres.firstOrNull()
+    println(primerValor)
+
+    listaVacia.removeAt(0)
+    println(listaVacia)
+
+    listaVacia.add("Diego")
+    println(listaVacia)
+    listaVacia.removeIf { caracteres -> caracteres.length > 3 }
+    println(listaVacia)
+
+    val myArray = arrayOf(1,2,3,4)
+    println("Nuestro array $myArray")
+    println("Array como lista ${myArray.toList()}")
+    //Los arrays son cuando se manejan miles de datos, si no usa listas.
 }
