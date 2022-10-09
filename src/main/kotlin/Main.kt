a5773fb83a52f6a3fcf15636a7aa09cd880e7463
diego <diegobadillo@live.com>
2022-10-09 11:37:48 -0500

    While, Do while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,14 @@
 
 fun main(args: Array<String>) {
-    val nombreColor = "Carmesi"
-
-    when (nombreColor) {
-        "Amarillo" -> println("EL amarillo es el color de los grillos")
-        "Rojo", "Carmesi" -> println("El color es de spiderman")
-        else -> println("Error. No tengo informacion del color ")
+    var contador = 10
+    while (contador > 0){
+        println("El valor del contador es $contador")
+        contador --
     }
 
-    val code = 405
-    when(code){
-        in 200..299 -> println("Todo ah ido bien")
-        in 400..500 -> println("ALgo ah fallado")
-        else -> println("Codigo desconocido, algo ah fallado.")
-    }
-
-    val tallaDeZapatos = 41
-    val mensaje = when(tallaDeZapatos){
-        41,43 -> "Tenemos disponible"
-        42,44 -> "Casi no nos quedan!"
-        45 -> "Lo siento no tenemos disponible"
-        else -> "Estos zapatos solo vienen en tallas 41-45"
-    }
-    println(mensaje)
+    do {
+        println("Generando numero aleatorio ...")
+        val numeroAleatorio = (0..100).random()
+        println("El numero generado es $numeroAleatorio")
+    } while (numeroAleatorio > 50)
 }
