9b187cbaa8b63585977671f51f783154c2f104a9
diego <diegobadillo@live.com>
2022-10-09 11:26:30 -0500

    When

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,26 @@
 
 fun main(args: Array<String>) {
-    val nombre = "Fer"
+    val nombreColor = "Carmesi"
 
-    if (nombre.isNotEmpty()) println("El  largo de nuestra variable nombre es ${nombre.length}") else println("Error la variable esta vacia")
+    when (nombreColor) {
+        "Amarillo" -> println("EL amarillo es el color de los grillos")
+        "Rojo", "Carmesi" -> println("El color es de spiderman")
+        else -> println("Error. No tengo informacion del color ")
+    }
 
-    val mensaje : String = if (nombre.length > 4 ) {
-        "Tu nombre es largo!"
-    } else if (nombre.isEmpty()) {
-        "Nombre esta vacio"
-    }  else {
-        "Tienes un nombre corto"
+    val code = 405
+    when(code){
+        in 200..299 -> println("Todo ah ido bien")
+        in 400..500 -> println("ALgo ah fallado")
+        else -> println("Codigo desconocido, algo ah fallado.")
+    }
+
+    val tallaDeZapatos = 41
+    val mensaje = when(tallaDeZapatos){
+        41,43 -> "Tenemos disponible"
+        42,44 -> "Casi no nos quedan!"
+        45 -> "Lo siento no tenemos disponible"
+        else -> "Estos zapatos solo vienen en tallas 41-45"
     }
     println(mensaje)
-
 }
