78f32d2ddd690046bfee682ed43c14fc499c7e8b
diego <diegobadillo@live.com>
2022-10-10 13:53:20 -0500

    Lambdas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    imprimirNombre(nombre = "Diego", apellido = "Badillo", )
-}
 
-fun imprimirNombre(nombre: String,segundoNombre: String = "", apellido: String){
-    println("Mi nombre es $nombre $segundoNombre y mi apellido es $apellido")
+fun main(args: Array<String>) {
+
+    // Como crear una lambda
+    val myLambda : (String) -> Int = {
+        it.length
+    }
+
+    val myLambda2 : (String) -> Int = { texto ->
+        texto.length * 2
+    }
+
+    // Como invocar una lambda
+    val resultadoDeLambda = myLambda("Hola Platzi")
+    println(resultadoDeLambda)
+
+    // Como usar una lambda como parámetro
+    val listaDeString = listOf("A","B","C")
+
+    val resultadoLambdaSinVariable = listaDeString.map { texto ->
+        texto.length * 2
+    }
+    println(resultadoLambdaSinVariable)
+
+    val resultadoLambda= listaDeString.map(myLambda2)
+    println(resultadoLambda)
+
 }
